c6b2796b0a2e200ed5685ba3b18762eebc96f9d1
Philipp Dargel <chisui@fb3.de>
2017-04-26 19:50:41 +0200

    litcache

--- ipasir/src/SAT/IPASIR/LiteralCache.hs
+++ ipasir/src/SAT/IPASIR/LiteralCache.hs
@@ -4,6 +4,7 @@ module SAT.IPASIR.LiteralCache where
 import qualified Data.Map as Map
 
 class LiteralCache (a :: * -> *) where
+    emptyCache :: forall l. a l
     numVars  :: forall l. a l -> Int
     intToVar :: forall l. a l -> Int -> l
     varToInt :: forall l. a l -> l -> Int
