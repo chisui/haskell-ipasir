a1c0c52296b7082d9f7bd22a97d74091c364da45
Philipp Dargel <chisui@fb3.de>
2017-05-05 17:46:40 +0200

    more useful tests

--- ipasir-cryptominisat/test/Spec.hs
+++ ipasir-cryptominisat/test/Spec.hs
@@ -1,16 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main (main) where
 
 import SAT.IPASIR
 import SAT.IPASIR.Cryptominisat
 import SAT.IPASIR.Cryptominisat.C
-
+import Data.Either
+import Control.Monad
 
 main :: IO ()
 main = do
     s <- new :: IO (Cryptominisat String)
-    s' <- addClauses s [[Pos "a", Pos "b", Pos "c"]]
-    (s'', solution) <- solve s'
-    print solution
-    putStrLn "\nBasic API works"
-
+    s' <- addClauses s (Some [
+            All [     "a",      "b", notB "c"],
+            All [     "a", notB "b", notB "c"],
+            All [     "a", notB "b",      "c"],
+            All [notB "a", notB "b", notB "c"]
+        ])
+    (_, solution) <- solve s'
+    unless (isLeft solution) $ error "should be solvable"
     
