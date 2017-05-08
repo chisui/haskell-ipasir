48935c4ed4648e1d84905875cf16c7a22c478479
Philipp Dargel <chisui@fb3.de>
2017-05-08 21:11:07 +0200

    Monoid is the answer

--- ipasir/src/SAT/IPASIR/StateStuff.hs
+++ ipasir/src/SAT/IPASIR/StateStuff.hs
@@ -39,33 +39,33 @@ class (Ord (StClausesLabel c), MSolver s) => IsMClause s (c :: *) where
     type StClausesLabel c
     mAddClauses :: (Ord l) => c -> s l -> IO (s l)
 
-    stAddClauses :: (Ord l, IsMClause s c) => c -> StateT (Maybe (s l)) IO ()
+    stAddClauses :: (Ord l, IsMClause s c) => c -> StateT (Eventual (s l)) IO ()
     stAddClauses clauses = do
-        (Just solver) <- get
+        (Present solver) <- get
         solver' <- lift $ mAddClauses clauses solver
-        put $ Just solver'
+        put $ Present solver'
 
 class MSolver (s :: * -> *) where
     newSolver :: (Ord l) => IO (s l)
     mSolve    :: (Ord l) => s l -> IO (ESolution l, s l)
     
-    stNewSolver :: (Ord l) => StateT (Maybe (s l)) IO ()
+    stNewSolver :: (Ord l) => StateT (Eventual (s l)) IO ()
     stNewSolver = do
         solver <- lift newSolver
-        put $ Just solver
+        put $ Present solver
     
-    stSolve :: (Ord l) => StateT (Maybe (s l)) IO (ESolution l)
+    stSolve :: (Ord l) => StateT (Eventual (s l)) IO (ESolution l)
     stSolve = do
-        (Just solver) <- get
+        (Present solver) <- get
         (solution, solver') <- lift $ mSolve solver
-        put $ Just solver'
+        put $ Present solver'
         return solution
         
 
 data CCryptominiSat = CCryptominiSat
 newtype MCryptoMiniSat l = MCryptoMiniSat CCryptominiSat
 
-newCryptominiSat :: (Ord l) => l -> StateT (Maybe (MCryptoMiniSat l)) IO ()
+newCryptominiSat :: (Ord l) => l -> StateT (Eventual (MCryptoMiniSat l)) IO ()
 newCryptominiSat _ = stNewSolver
 
 instance MSolver MCryptoMiniSat where
@@ -81,23 +81,23 @@ instance (Ord l) => IsMClause MCryptoMiniSat [[Lit l]] where
     type StClausesLabel [[Lit l]] = l
     mAddClauses = undefined
 
-noSolver = Nothing
+data Eventual a 
+    = NotYet
+    | Present a
 
-class HasEmpty a where
-    empty :: a
-instance HasEmpty (Maybe a) where
-    empty = Nothing
-instance (HasEmpty a, HasEmpty b) => HasEmpty (a,b) where
-    empty = (empty, empty)
+instance Monoid (Eventual a) where
+    mempty = NotYet
+    mappend _ e@(Present _) = e
+    mappend e _ = e
 
-runSolver :: (HasEmpty a, Monad m) => StateT a m b -> m b
-runSolver s = evalStateT s empty 
+runSolver :: (Monoid a, Monad m) => StateT a m b -> m b
+runSolver s = evalStateT s mempty 
 
 main :: IO ()
-main = evalStateT operation (noSolver, noSolver)
+main = runSolver operation
 
 
-operation :: StateT (Maybe (MCryptoMiniSat String), Maybe (MCryptoMiniSat String)) IO ()
+operation :: StateT (Eventual (MCryptoMiniSat String), Eventual (MCryptoMiniSat String)) IO ()
 operation = do
     _2 =.= newCryptominiSat (undefined :: String)
     _1 =.= stAddClauses [[Pos ""]]
