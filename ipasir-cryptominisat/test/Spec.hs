9230272c59a517d254a961b583f908cecc302365
Philipp Dargel <chisui@fb3.de>
2017-05-19 13:35:23 +0200

    stuff

--- ipasir-cryptominisat/test/Spec.hs
+++ ipasir-cryptominisat/test/Spec.hs
@@ -8,13 +8,19 @@ import Control.Monad
 
 main :: IO ()
 main = do
-    s <- new :: IO (Cryptominisat String)
+    s <- new :: IO (Minisat String)
+    f sa
+data Card = 
+
+f :: Solver s => s String -> IO ()
+f s = do
     s' <- addClauses s (Some [
             All [     "a",      "b", notB "c"],
             All [     "a", notB "b", notB "c"],
             All [     "a", notB "b",      "c"],
             All [notB "a", notB "b", notB "c"]
         ])
+    addClauses s' (count ....) 
     s'' <- addClauses s' (Even ["a", "b", "c", "x", "y", "z"])
     (_, solution) <- solve s''
     unless (isLeft solution) $ error "should be solvable"
--- ipasir/src/SAT/IPASIR/StateStuff.hs
+++ ipasir/src/SAT/IPASIR/StateStuff.hs
@@ -5,12 +5,16 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE DeriveFunctor #-}
 module SAT.IPASIR.StateStuff where
 
+import Data.Traversable
 import Control.Monad.Trans.State.Lazy
 import Control.Monad.Trans.Class
 import Control.Monad.Primitive
 import Control.Lens
+
+import Data.Monoid
 
 import qualified Data.Map as M
 
@@ -39,33 +43,31 @@ class (Ord (StClausesLabel c), MSolver s) => IsMClause s (c :: *) where
     type StClausesLabel c
     mAddClauses :: (Ord l) => c -> s l -> IO (s l)
 
-    stAddClauses :: (Ord l, IsMClause s c) => c -> StateT (Eventual (s l)) IO ()
+    stAddClauses :: (Traversable m, Ord l, IsMClause s c) => c -> StateT (m (s l)) IO ()
     stAddClauses clauses = do
-        (Present solver) <- get
-        solver' <- lift $ mAddClauses clauses solver
-        put $ Present solver'
+        solver  <- get
+        solver' <- lift $ mapM (mAddClauses clauses) solver
+        put solver'
 
 class MSolver (s :: * -> *) where
     newSolver :: (Ord l) => IO (s l)
     mSolve    :: (Ord l) => s l -> IO (ESolution l, s l)
     
-    stNewSolver :: (Ord l) => StateT (Eventual (s l)) IO ()
-    stNewSolver = do
-        solver <- lift newSolver
-        put $ Present solver
+    stNewSolver :: (Applicative m, Ord l) => StateT (m (s l)) IO ()
+    stNewSolver = put . pure =<< lift newSolver
     
-    stSolve :: (Ord l) => StateT (Eventual (s l)) IO (ESolution l)
+    stSolve :: forall m l. (Traversable m, Applicative m, Ord l) => StateT (m (s l)) IO (m (ESolution l))
     stSolve = do
-        (Present solver) <- get
-        (solution, solver') <- lift $ mSolve solver
-        put $ Present solver'
-        return solution
-        
+        solver <- get
+        do 
+            r <- lift $ mapM mSolve solver :: StateT (m (s l)) IO (m (ESolution l, s l))
+            mapM (put . pure . snd) r
+            mapM (return . fst) r
 
 data CCryptominiSat = CCryptominiSat
 newtype MCryptoMiniSat l = MCryptoMiniSat CCryptominiSat
 
-newCryptominiSat :: (Ord l) => l -> StateT (Eventual (MCryptoMiniSat l)) IO ()
+newCryptominiSat :: (Applicative m, Monoid (m (MCryptoMiniSat l)), Ord l) => l -> StateT (m (MCryptoMiniSat l)) IO ()
 newCryptominiSat _ = stNewSolver
 
 instance MSolver MCryptoMiniSat where
@@ -81,15 +83,6 @@ instance (Ord l) => IsMClause MCryptoMiniSat [[Lit l]] where
     type StClausesLabel [[Lit l]] = l
     mAddClauses = undefined
 
-data Eventual a 
-    = NotYet
-    | Present a
-
-instance Monoid (Eventual a) where
-    mempty = NotYet
-    mappend _ e@(Present _) = e
-    mappend e _ = e
-
 runSolver :: (Monoid a, Monad m) => StateT a m b -> m b
 runSolver s = evalStateT s mempty 
 
@@ -97,7 +90,7 @@ main :: IO ()
 main = runSolver operation
 
 
-operation :: StateT (Eventual (MCryptoMiniSat String), Eventual (MCryptoMiniSat String)) IO ()
+operation :: StateT (Last (MCryptoMiniSat String), Last (MCryptoMiniSat String)) IO ()
 operation = do
     _2 =.= newCryptominiSat (undefined :: String)
     _1 =.= stAddClauses [[Pos ""]]
