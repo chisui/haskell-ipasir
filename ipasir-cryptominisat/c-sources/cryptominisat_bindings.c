8c23a211001ea7be3514a7c9ae41c84d14ebd995
Philipp Dargel <chisui@fb3.de>
2017-06-08 18:57:48 +0200

    fixed tests

--- ipasir-cryptominisat/c-sources/cryptominisat_bindings.c
+++ ipasir-cryptominisat/c-sources/cryptominisat_bindings.c
@@ -16,11 +16,12 @@ void crypto_add_xor_clause(void* rawSolver, void* vars, size_t num_vars, bool rh
         printf("%d ", lit);
         maxLit = max(maxLit, lit);
     }
-    printf("]\n", maxLit);
-
-    unsigned currMax = cmsat_nvars(satSolver) - 1;
+    printf("] max=%d\n", maxLit);
+    unsigned currMax = cmsat_nvars(satSolver);
     if (maxLit > currMax) {
-        cmsat_new_vars(satSolver, maxLit - currMax);
+        unsigned newVars = 1 + (maxLit - currMax);
+        printf("add %d\n", newVars);
+        cmsat_new_vars(satSolver, newVars);
     }
     
     cmsat_add_xor_clause(satSolver, vars, num_vars, rhs);
