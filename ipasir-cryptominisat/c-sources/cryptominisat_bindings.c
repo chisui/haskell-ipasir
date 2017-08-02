0a01b74182156b080f6b6d03d992c163eeeebcdd
Gerrit <tirreg@math.uni-bremen.de>
2017-08-02 21:30:09 +0200

    binding neu

--- ipasir-cryptominisat/c-sources/cryptominisat_bindings.c
+++ ipasir-cryptominisat/c-sources/cryptominisat_bindings.c
@@ -5,27 +5,27 @@
     #define max(a,b) ((a) > (b) ? (a) : (b))
 #endif
 
-void crypto_add_xor_clause(void* rawSolver, void* vars, size_t num_vars, bool rhs) {
-    // printf("crypto_add_xor_clause [");
-    SATSolver* satSolver = get_real_solver(rawSolver);
+/*
+  Every vector consists of 3 pointer. 
+*/
+const int vector_size=3*sizeof( int32_t* );
+const int offset =2*vector_size;
 
+void crypto_add_xor_clause(void* rawSolver, void* vars, size_t num_vars, bool rhs) {
+
+    SATSolver* satSolver = * ((SATSolver**) (rawSolver+offset));
+    int i;
     int maxLit = 0;
     unsigned* vec = (unsigned*) vars;
-    int i;
     for (i=0; i<num_vars; i++) {
         int lit = vec[i];
-//        printf("%d ", lit);
         maxLit = max(maxLit, lit);
     }
     int currMax = cmsat_nvars(satSolver);
-//    printf("] max=%d,    where currMax=%d\n", maxLit,currMax);
 
     if (maxLit >= currMax) { // >= to remove out of 1 error. The Vector vars starts at 0 (so maxLit is increased by 1)
         int newVars = 1 + (maxLit - currMax);
-
-    //    printf("add %d,   Max before: %d,    " , newVars, currMax);
         cmsat_new_vars(satSolver, newVars);
-    //    printf("Max after: %d\n" , cmsat_nvars(satSolver));
     }
 
     cmsat_add_xor_clause(satSolver, vars, num_vars, rhs);
